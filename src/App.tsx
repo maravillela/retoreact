79fd49a649a00e134385feb2a898cb2f267a6fc5
maravillela <mara_473@hotmail.com>
2023-07-17 18:17:55 -0700

    agregue cosas en side izq

--- src/App.tsx
+++ src/App.tsx
@@ -72,6 +72,10 @@ function App() {
 <ul>📯 Contact</ul>
 <ul>📖 Guides</ul>
 <ul>🤔 Software comparisons</ul>
+<ul className="font-bold pt-5 text-black">Other</ul>
+<ul>👍🏻 Code of Conduct</ul>
+<ul>🤓 Privacy Policy</ul>
+<ul>👀 Terms of use</ul>
 
 <div className="pt-5">
 <div className="card card-compact p-3 w-50 h-80 bg-primary-content shadow-xl">
@@ -192,11 +196,12 @@ function App() {
 </div>
 </div>
 
-<div className="card w-50 h-80 bg-primary-content shadow-xl">
+<div className="card card-compact w-50 h-80 bg-primary-content shadow-xl">
   <figure><img src="https://res.cloudinary.com/practicaldev/image/fetch/s--sHHbPqt4--/c_limit,f_auto,fl_progressive,q_auto,w_500/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ylq17e4fm8gfs75ur7nf.jpg" alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title text-black text-sm">Refine + DEV Hackathon</h2>
     <p className="text-xs">RSVQuick: Online Invitation App Made Using Refine</p>
+    <p className="text-xs">Inventory Management Website</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary btn-block text-xs">Share your project</button>
     </div>
